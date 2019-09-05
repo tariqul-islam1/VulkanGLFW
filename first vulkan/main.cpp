a2dac8fe0ba399ef31f1ab521259efcfaabbb87c
Cool Brain <coolbrain@Cools-MacBook-Pro.local>
2019-09-05 11:49:11 +0200

    Initial Commit

---
+++ first vulkan/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  first vulkan
+//
+//  Created by Cool Brain on 9/5/19.
+//  Copyright © 2019 Cool Brain. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
